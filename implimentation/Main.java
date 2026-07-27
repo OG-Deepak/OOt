f384afc13606f1135c3ac7be55544b7b8aee871c
OG-Deepak <deepakdevan458@gmail.com>
2026-07-27 20:13:48 +0530

    new implemenation folder

---
+++ implimentation/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Customer s = new Customer();
+        s.register(1, "name", 11111, "whatup@gmail.com");
+        s.view();
+        System.out.println("do u want to update values");
+        
+    }
+}
