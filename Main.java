82cb5a8f3ae286c917058c8927be64fb6fbe53c9
OG-Deepak <deepakdevan458@gmail.com>
2026-07-27 20:15:50 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Customer s = new Customer();
-        s.register(1, "name", 11111, "whatup@gmail.com");
-        s.view();
-        System.out.println("do u want to update values");
-        
-    }
-}
